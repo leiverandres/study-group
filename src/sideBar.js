f088dd98fa2fb5057994b0ce837fb2a40200a57c
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-12-06 23:50:43 -0500

    Add icons to side bar and app.json

--- src/sideBar.js
+++ src/sideBar.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import { Image } from "react-native"
+import React, { Component } from 'react';
+import { Image } from 'react-native';
 
 import {
   Content,
@@ -12,34 +12,32 @@ import {
   Right,
   Badge,
   Button,
-  View,
-  StyleProvider,
-  getTheme,
-  variables
-} from "native-base"
+  View
+} from 'native-base';
+const drawerCover = require('./img/logo_with_name.png');
 
 const datas = [
   {
-    name: "Anatomy",
-    route: "Anatomy",
-    icon: "phone-portrait",
-    bg: "#C5F442"
+    name: 'Anatomy',
+    route: 'Anatomy',
+    icon: 'phone-portrait',
+    bg: '#C5F442'
   },
   {
-    name: "Actionsheet",
-    route: "Actionsheet",
-    icon: "easel",
-    bg: "#C5F442"
+    name: 'Actionsheet',
+    route: 'Actionsheet',
+    icon: 'easel',
+    bg: '#C5F442'
   }
-]
-
+];
+const deviceHeight = Dimensions.get('window').height;
 class SideBar extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       shadowOffsetWidth: 1,
       shadowRadius: 4
-    }
+    };
   }
 
   render() {
@@ -47,45 +45,62 @@ class SideBar extends Component {
       <Container>
         <Content
           bounces={false}
-          style={{ flex: 1, backgroundColor: "#fff", top: -1 }}
+          style={{ flex: 1, backgroundColor: '#fff', top: -1 }}
         >
-          <List
-            dataArray={datas}
-            renderRow={data => (
-              <ListItem
-                button
-                noBorder
-                onPress={() => this.props.navigation.navigate(data.route)}
-              >
-                <Left>
-                  <Icon
-                    active
-                    name={data.icon}
-                    style={{ color: "#777", fontSize: 26, width: 30 }}
-                  />
-                  <Text>{data.name}</Text>
-                </Left>
-                {data.types && (
-                  <Right style={{ flex: 1 }}>
-                    <Badge
-                      style={{
-                        borderRadius: 3,
-                        height: 25,
-                        width: 72,
-                        backgroundColor: data.bg
-                      }}
-                    >
-                      <Text>{`${data.types} Types`}</Text>
-                    </Badge>
-                  </Right>
-                )}
-              </ListItem>
-            )}
-          />
+          <View style={{ flex: 1 }}>
+            <Image
+              source={{
+                uri:
+                  'https://firebasestorage.googleapis.com/v0/b/study-group-57d2c.appspot.com/o/logo_with_name.png?alt=media&token=779fc20d-6eda-4b76-9e78-c7e640a65832'
+              }}
+              style={{
+                alignSelf: 'stretch',
+                height: deviceHeight / 3.5,
+                width: null,
+                position: 'relative',
+                marginBottom: 10
+              }}
+            />
+          </View>
+          <View style={{ flex: 6 }}>
+            <List
+              dataArray={datas}
+              renderRow={data => (
+                <ListItem
+                  button
+                  noBorder
+                  onPress={() => this.props.navigation.navigate(data.route)}
+                >
+                  <Left>
+                    <Icon
+                      active
+                      name={data.icon}
+                      style={{ color: '#777', fontSize: 26, width: 30 }}
+                    />
+                    <Text>{data.name}</Text>
+                  </Left>
+                  {data.types && (
+                    <Right style={{ flex: 1 }}>
+                      <Badge
+                        style={{
+                          borderRadius: 3,
+                          height: 25,
+                          width: 72,
+                          backgroundColor: data.bg
+                        }}
+                      >
+                        <Text>{`${data.types} Types`}</Text>
+                      </Badge>
+                    </Right>
+                  )}
+                </ListItem>
+              )}
+            />
+          </View>
         </Content>
       </Container>
-    )
+    );
   }
 }
 
-export default SideBar
+export default SideBar;
